ae0883f3c9c7ed24d8193ff85d1576c52c8b9bff
Mariana <lmarianarp@gmail.com>
2018-01-04 21:10:01 -0800

    add models to the app

--- myMatchingApp/match/models.py
+++ myMatchingApp/match/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Community(models.Model):
+    number_couples: models.IntegerField()
+
+class Woman(models.Model):
+    name =  models.CharField(max_length=50)
+    community = models.ForeignKey('Community', on_delete=models.CASCADE)
+    pairing = models.ForeignKey('Pairing', on_delete=models.CASCADE)
+
+class Man(models.Model):
+    name = models.CharField(max_length=50)
+    community = models.ForeignKey('Community', on_delete=models.CASCADE)
+    pairing = models.ForeignKey('Pairing', on_delete=models.CASCADE)
+
+class Ranking(models.Model):
+    woman_score = models.IntegerField()
+    man_score = models.IntegerField()
+    woman = models.ForeignKey('Woman', on_delete=models.CASCADE)
+    man = models.ForeignKey('Man', on_delete=models.CASCADE)
+
+class Pairing(models.Model):
+    matching = models.ForeignKey('Matching', on_delete=models.CASCADE)
+
+class Matching(models.Model):
+    community = models.ForeignKey('Community', on_delete=models.CASCADE)
+    algorithm = models.CharField(max_length=20)
