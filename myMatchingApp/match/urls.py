fce839e02e722c63df52725d0e2f019dbf90bd6a
Mariana <lmarianarp@gmail.com>
2018-01-05 14:58:02 -0800

    Add a template for the communities

--- myMatchingApp/match/urls.py
+++ myMatchingApp/match/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/community', views.community_list, name='community_list'),
+    path('community/', views.community_list, name='community_list'),
 ]
--- myMatchingApp/match/views.py
+++ myMatchingApp/match/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Community
+
 
 # Create your views here.
+def community_list(request):
+    communities = Community.objects.all()
+    return render(request, 'match/community_list.html', {'communities': communities})
+
+
+
+
+
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
