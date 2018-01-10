603d87dd8df79cf13f6c254dd6f91b782e6e4482
Mariana <lmarianarp@gmail.com>
2018-01-09 16:29:16 -0800

    edit and delete button for community

--- myMatchingApp/match/urls.py
+++ myMatchingApp/match/urls.py
@@ -5,8 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('community/', views.community_list, name='community_list'),
-    path('community/new', views.new_community, name='new_community'),
+    # path('community/new', views.new_community, name='new_community'),
     path('new_red', views.new_red, name='new_red'),
     path('new_blue', views.new_blue, name='new_blue'),
+    path('community/create/', views.CommunityCreate.as_view(), name='community_create'),
+    path('community/<int:pk>/update/', views.CommunityUpdate.as_view(), name='community_update'),
+    path('commit/<int:pk>/delete/', views.CommunityDelete.as_view(), name='community_delete'),
 
 ]
--- myMatchingApp/match/views.py
+++ myMatchingApp/match/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Community
 from .forms import CommunityForm, RedForm, BlueForm
 from django.shortcuts import redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -15,18 +17,18 @@ def community_list(request):
     communities = Community.objects.all()
     return render(request, 'match/community_list.html', {'communities': communities})
 
-def new_community(request):
-    if request.method == "POST":
-        form = CommunityForm(request.POST)
-        if form.is_valid():
-            community = form.save(commit=False)
-            community.save()
-            # return redirect('new_woman', id=community.id)
-            return redirect('home')
-            # return redirect('community_list')
-    else:
-        form = CommunityForm()
-    return render(request, 'match/new_community.html', {'form': form})
+# def new_community(request):
+#     if request.method == "POST":
+#         form = CommunityForm(request.POST)
+#         if form.is_valid():
+#             community = form.save(commit=False)
+#             community.save()
+#             # return redirect('new_woman', id=community.id)
+#             return redirect('home')
+#             # return redirect('community_list')
+#     else:
+#         form = CommunityForm()
+#     return render(request, 'match/new_community.html', {'form': form})
 
 def new_red(request):
     if request.method == "POST":
@@ -50,6 +52,19 @@ def new_blue(request):
     else:
         form = BlueForm()
     return render(request, 'match/new_blue.html', {'form': form})
+
+class CommunityCreate(CreateView):
+    model = Community
+    fields = '__all__'
+    # initial={'date_of_death':'05/01/2018',}
+
+class CommunityUpdate(UpdateView):
+    model = Community
+    fields = ['name']
+
+class CommunityDelete(DeleteView):
+    model = Community
+    success_url = reverse_lazy('community_list')
 
 
 # def index(request):
